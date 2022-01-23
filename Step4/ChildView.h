a4e96313e1624b9606afef5d0b371d1b10d2d257
Tony Froman <33915703+fromanan@users.noreply.github.com>
2022-01-23 15:56:46 -0500

    Initial commit

---
+++ Step4/ChildView.h
@@ -0,0 +1,60 @@
+
+// ChildView.h : interface of the CChildView class
+//
+
+
+#pragma once
+#include "ShaderWnd/ShaderWnd.h"
+
+#include "Mesh.h"
+#include "Sphere.h"
+#include "Torus.h"
+#include "graphics\GrTexture.h"
+#include "graphics\GrCubeTexture.h"
+
+
+// CChildView window
+
+class CChildView : public CShaderWnd
+{
+// Construction
+public:
+	CChildView();
+
+// Attributes
+public:
+
+// Operations
+public:
+
+// Overrides
+	protected:
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+
+// Implementation
+public:
+	virtual ~CChildView();
+
+	// Generated message map functions
+protected:
+	DECLARE_MESSAGE_MAP()
+
+public:
+	virtual void RenderGL();
+	virtual void InitGL();
+	virtual void CleanGL();
+
+private:
+	CGrTexture m_bunnyTex;
+	CGrTexture m_fishTex;
+	CGrTexture m_sphereTex;
+	CGrTexture m_torusTex;
+	CGrCubeTexture m_cubeTex;
+	CMesh m_bunny;
+	CMesh m_fish;
+	CSphere m_sphere;
+	CSphere m_metallicSphere;
+	CTorus m_torus;
+	CMesh m_skybox;
+};
+
