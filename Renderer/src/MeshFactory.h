30dc4d449bd90d37b64d60cd3e98bbb10f456fb7
Tony Froman <33915703+fromanan@users.noreply.github.com>
2023-09-25 10:19:05 -0400

    Added MeshFactory

---
+++ Renderer/src/MeshFactory.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "ShaderHeaders.h"
+
+#include "ShaderWnd.h"
+
+#include "Primitives/Sphere.h"
+#include "Primitives/Torus.h"
+#include "Mesh.h"
+
+class MeshFactory
+{
+public:
+    static std::shared_ptr<CMesh> CreateCube(CShaderWnd* window, const std::string& vertexShader, const std::string& fragmentShader)
+    {
+        auto cube = std::make_shared<CMesh>();
+        cube->CreateCube();
+        cube->m_program = window->LoadShaders(vertexShader, fragmentShader);
+        return cube;
+    }
+    
+    static std::shared_ptr<CMesh> CreateMesh(CShaderWnd* window, const std::string& vertexShader, const std::string& fragmentShader, const std::string& filename)
+    {
+        auto mesh = std::make_shared<CMesh>();
+        mesh->LoadOBJ(filename.c_str());
+        mesh->m_program = window->LoadShaders(vertexShader, fragmentShader);
+        return mesh;
+    }
+    
+    static std::shared_ptr<CSphere> CreateSphere(CShaderWnd* window, const std::string& vertexShader, const std::string& fragmentShader, const float radius)
+    {
+        auto sphere =  std::make_shared<CSphere>();
+        sphere->SetRadius(radius);
+        sphere->m_program = window->LoadShaders(vertexShader, fragmentShader);
+        return sphere;
+    }
+    
+    static std::shared_ptr<CTorus> CreateTorus(CShaderWnd* window, const std::string& vertexShader, const std::string& fragmentShader, const float radius1, const float radius2)
+    {
+        auto torus =  std::make_shared<CTorus>();
+        //cube.SetR1(3);
+        //cube.SetR2(3);
+        torus->m_program = window->LoadShaders(vertexShader, fragmentShader);
+        return torus;
+    }
+};
